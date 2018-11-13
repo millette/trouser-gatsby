0d7dee7c3104fd7171859dd262d388457bd95bbc
Robin Millette <robin@millette.info>
2018-11-13 17:46:11 -0500

    Normalize rows.

--- src/components/file.js
+++ src/components/file.js
@@ -3,6 +3,25 @@ import React, { Component } from 'react'
 // self
 import JsonFile from '../components/json-file'
 
+const normalize = (x) => {
+  if (!x.length) {
+    throw new Error('Invalid file')
+  }
+
+  const keys = x
+    .map((o) => Object.keys(o))
+    .reduce((a, b) => [...a, ...b], [])
+
+  const z = Array.from(new Set(keys)).sort()
+  return x.map((b) => {
+    const ret = { }
+    z.forEach((a) => {
+      ret[a] = b[a]
+    })
+    return ret
+  })
+}
+
 export default class File extends Component {
   constructor(props) {
     super(props)
@@ -15,8 +34,12 @@ export default class File extends Component {
     }
 
     try {
+      this.setState({ json: false, error: 'read...' })
       const res = await fetch(this.props.publicURL)
-      const json = await res.json()
+      this.setState({ error: 'json...' })
+      const json2 = await res.json()
+      this.setState({ error: 'norm...' })
+      const json = normalize(json2)
       this.setState({ json, error: false })
     } catch (e) {
       this.setState({ json: false, error: e.toString() })
