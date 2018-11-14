73d1f7b11d33426a3b8c8ed1531231368f213a03
Robin Millette <robin@millette.info>
2018-11-14 14:34:56 -0500

    Reformat.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
-'use strict'
+"use strict"
 
 // self
-const { name, version } = require('./package.json')
+const { name, version } = require("./package.json")
 
 module.exports = {
   siteMetadata: {
     title: `${name} v${version}`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'le-f2',
+        name: "le-f2",
         path: `${__dirname}/custom/f2/`,
       },
     },
-    'gatsby-plugin-no-sourcemaps',
+    "gatsby-plugin-no-sourcemaps",
   ],
 }
