fb4fee730fbb732d372bc0e852d38c4099da28f1
Robin Millette <robin@millette.info>
2018-11-14 09:51:17 -0500

    No source maps.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,21 @@
+'use strict'
+
+// self
+const { name, version } = require('./package.json')
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: `${name} v${version}`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    /*
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'le-f5',
-        path: `${__dirname}/custom/f5/`
-      },
-    },
-    */
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'le-f2',
-        path: `${__dirname}/custom/f2/`
+        path: `${__dirname}/custom/f2/`,
       },
-    }
-  ]
+    },
+    'gatsby-plugin-no-sourcemaps',
+  ],
 }
